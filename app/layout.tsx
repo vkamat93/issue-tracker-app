63d057a2e56d6059d0f9d167c2c1851760ee17f5
vkamat93 <vikrant.p.kamat@gmail.com>
2025-09-16 22:29:26 +0530

    customize Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import '@radix-ui/themes/styles.css'
+import './theme-config.css'
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Inter } from "next/font/google";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({
+  subsets: ['latin'],
+  variable: "--font-inter",
+})
 
 
 export const metadata: Metadata = {
@@ -21,10 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
-        <Navbar />
-        <main className="p-5">{children}</main>
+      <body className={inter.variable}>
+        <Theme accentColor="violet">
+          <Navbar />
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
