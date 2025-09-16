6e969a95212a24e4cdf8afeee2414449429740f2
vkamat93 <vikrant.p.kamat@gmail.com>
2025-09-16 19:36:36 +0530

    create issue model

---
+++ prisma/migrations/20250916140410_create_issue/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "public"."Status" AS ENUM ('OPEN', 'IN_PROGRESS', 'CLOSED');
+
+-- CreateTable
+CREATE TABLE "public"."Issue" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "public"."Status" NOT NULL DEFAULT 'OPEN',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Issue_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Issue_title_key" ON "public"."Issue"("title");
